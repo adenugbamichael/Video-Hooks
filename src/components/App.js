9f2dc29541e717e50b8f8fb9941909ee58e186be
adenugbamichael <adenugbamicke@gmail.com>
2023-03-19 16:48:40 +0100

    extract video logic | using custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,27 @@
 import React, { useState, useEffect } from "react"
 import SearchBar from "./SearchBar"
-import youtube from "../apis/youtube"
 import VideoList from "./VideoList"
 import VideoDetail from "./VideoDetail"
+import useVideos from "../hooks/useVideos"
 
 const App = () => {
-  const [videos, setVideos] = useState([])
   const [selectedVideo, setSelectedVideo] = useState(null)
+  const [videos, search] = useVideos("buildings")
 
   useEffect(() => {
-    onTermSubmit("buildings")
-  }, [])
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    })
-
-    setVideos(response.data.items)
-    setSelectedVideo(response.data.items[0])
-  }
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video)
-  }
+    setSelectedVideo(videos[0])
+  }, [videos])
 
   return (
     <div className='ui container'>
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className='ui grid'>
         <div className='ui row'>
           <div className='eleven wide column'>
             <VideoDetail video={selectedVideo} />
           </div>
           <div className='five wide column'>
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
